026a53d02cf359e8fc9743848e959b04cc60c7df
jbeckhaus <jonathan.beckhaus@live.de>
2018-04-24 19:29:49 +0200

    add arduino serial dings

---
+++ arduino_Serial_Sniffer/arduino_Serial_Sniffer.ino
@@ -0,0 +1,19 @@
+#include <SoftwareSerial.h>
+
+const byte rx = 2;
+const byte tx = 3;
+
+SoftwareSerial myserial(rx, tx);
+
+void setup() {
+  Serial.begin(9600);
+  myserial.begin(9600);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.write(myserial.read());
+  //Serial.write("HelloWorld");
+  //delay(1000);
+}
