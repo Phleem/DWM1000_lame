25cd0dc206de07052093505320c709008357ff4c
jbeckhaus <jonathan.beckhaus@live.de>
2018-04-27 11:56:33 +0200

    hartbead led (ok status led should be on by now)

--- blinky/main.cpp
+++ blinky/main.cpp
@@ -4,8 +4,9 @@
 
 Serial pc(PA_9, PA_10); // tx, rx
 SPI spi(PA_7, PA_6 ,PA_5) //MOSI, MISO, SerialClock
-_
+
 DigitalOut cs(PA_4); //ChipSelect
+DigitalOut irq(PA_0);
 
 DigitalOut Pin0(PB_0) ;
 DigitalOut Pin1(PB_1) ;
@@ -14,6 +15,15 @@ DigitalOut Pin1(PB_1) ;
 // main() runs in its own thread in the OS
 int main() {
 	cs = 1; //deselect chip	
+	
+	DW1000(spi, irq, cs, NC);   
+	
+	DW1000.writeRegister40( DW1000_GPIO_CTRL , 0x00, 0x00); // Set GPIO as GPIO (no special function)
+	DW1000.writeRegister40( DW1000_GPIO_CTRL , 0x08, 0x00); // Set GPIO DirectionRegister to output 
+    DW1000.writeRegister40( DW1000_GPIO_CTRL , 0x0C, 0x01); // Set GPIO 1 to HIGH
+	
+	
+	
 	
 	
     while (true) {
