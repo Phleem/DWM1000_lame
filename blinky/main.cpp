56c3ae69772c6682449fbb4e177d5b5899525f2c
meelph <ThousandPies@web.de>
2018-04-27 11:00:29 +0200

    added docu; setup main

--- blinky/main.cpp
+++ blinky/main.cpp
@@ -1,38 +1,32 @@
 #include "mbed.h"
-Serial pc(PA_9, PA_10); // tx, rx
+#include "DW1000Utils.h"
+#include "DW1000.h"
 
-//DigitalOut led1(LED1);
+Serial pc(PA_9, PA_10); // tx, rx
+SPI spi(PA_7, PA_6 ,PA_5) //MOSI, MISO, SerialClock
+_
+DigitalOut cs(PA_4); //ChipSelect
 
 DigitalOut Pin0(PB_0) ;
 DigitalOut Pin1(PB_1) ;
-DigitalOut Pin2(PB_2) ;
-DigitalOut Pin3(PB_3) ;
-DigitalOut Pin4(PB_4) ;
-DigitalOut Pin5(PB_5) ;
-DigitalOut Pin6(PB_6) ;
 
 
 // main() runs in its own thread in the OS
 int main() {
+	cs = 1; //deselect chip	
+	
+	
     while (true) {
         Pin1 = 1;
         Pin0 = 1;
-        Pin2 = 1;
-        Pin3 = 1;
-        Pin4 = 1;
-        Pin5 = 1;
-        Pin6 = 1;
+    
         //led1 = !led1;
         wait(1);
         
         
         Pin1 = 0;
         Pin0 = 0;
-        Pin2 = 0;
-        Pin3 = 0;
-        Pin4 = 0;
-        Pin5 = 0;
-        Pin6 = 0;
+      
         wait(1);
         pc.printf("Hello World!\n");
 
